3b99bafbb8245f674989963fa71f169cc1687480
Atsuya Nishiyama <a-nishiyama@mldfp.com>
2025-10-08 16:12:15 +0900

    routerの設定とMainアイコンの表示

---
+++ src/components/pages/TDLogin.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TDMainMark from '@/components/TDMainMark/TDMainMark.vue';
+
+</script>
+
+<template>
+    <h1>このページはTDLogin.vueです。</h1>
+    <button @click="$router.push('/home')">Homeに戻る</button>
+    <TDMainMark src="../public/images/TDMainMark.svg"/>
+</template>
