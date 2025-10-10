b43caddb31e3b1b632f0eadbbb4189bf3548b81c
Atsuya Nishiyama <a-nishiyama@mldfp.com>
2025-10-10 09:48:27 +0900

    TDinput.vueの完成

--- src/components/TDInput/TDInput.vue
+++ src/components/TDInput/TDInput.vue
@@ -1,75 +1,59 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+const value = defineModel<string>({ required: true })
 
-// Propsの型定義
-interface TDInputProps {
-    modelValue: string
+const visible = defineModel<boolean>('visible', { default: false })
+
+interface inputData {
     error?: boolean
     type: 'mail' | 'password'
-    visible?: boolean
     openedEyeSrc?: string
     closedEyeSrc?: string
 }
 
-// Emitsの型定義
-type TDInputEmits = {
-    (e: 'update:modelValue', value: string): void
-}
-
-// PropsとEmitsの定義
-const props = defineProps<TDInputProps>()
-const emit = defineEmits<TDInputEmits>()
-
-const visible = ref(false)
-
-
-// ユーザーが入力値が変更したときに発火する関数
-const onInput = (e: Event) => {
-    const target = e.target as HTMLInputElement
-    emit('update:modelValue', target.value)
-}
+const data = defineProps<inputData>()
 </script>
 
 <template>
     <div class="_input_group">
         <!-- メールアドレス入力 -->
         <!-- typeがmailのときのみ表示 -->
-        <div v-if="props.type === 'mail'" class="_input_form _input_mail_form">
+        <div v-if="data.type === 'mail'" class="_input_form _input_mail_form">
             <label for="email" class="_email_label">メールアドレス</label>
             <input 
                 type="email"   
                 id="email" 
                 name="email" 
-                v-model="props.modelValue"
+                v-model="value"
                 class="_input_group_text _email_input"
-                :class="{'is-invalid': props.error}"
-                @input="onInput"
+                :class="{'_error_color': data.error}"
             />
         </div>
         
         <!-- パスワード入力 -->
         <!-- typeがpasswordのときのみ表示 -->
-        <div v-if="props.type === 'password'" class="_input_form _input_password_form">
+        <div v-if="data.type === 'password'" class="_input_form _input_password_form">
             <label for="password" class="_password_label">パスワード</label>
             <input 
-                type="password" 
+                :type="visible ? 'text' : 'password'" 
                 id="password" 
                 name="password" 
-                v-model="props.modelValue"
+                v-model="value"
                 class="_input_group_text _password_input"
-                :class="{'is-invalid': props.error}"
-                @input="onInput"
+                :class="{
+                    '_error_color': data.error,
+                    '_password_masked': !visible
+                }"
             />
             <button @click="visible = !visible" class="_eye_button">
                 <img 
-                    v-if="!visible" 
-                    :src="props.openedEyeSrc" 
+                    v-if="visible" 
+                    :src="data.openedEyeSrc" 
                     alt="目が開いたアイコン" 
                     class="_opened_eye_icon"
                 />
                 <img 
                     v-else 
-                    :src="props.closedEyeSrc" 
+                    :src="data.closedEyeSrc" 
                     alt="目が閉じたアイコン"
                     class="_closed_eye_icon"
                 />
@@ -79,6 +63,27 @@ const onInput = (e: Event) => {
 </template>
 
 <style lang="sass" scoped>
+    .input 
+        margin: 0
+        padding: 0
+        background: none
+        border: none
+        border-radius: 0
+        outline: none   
+        -webkit-appearance: none
+        -moz-appearance: none
+        appearance: none
+    .button
+        margin: 0
+        padding: 0
+        background: none
+        border: none
+        border-radius: 0
+        outline: none
+        -webkit-appearance: none
+        -moz-appearance: none
+        appearance: none
+        cursor: pointer
     ._input_group
         font-family: 'Noto Sans JP', sans-serif
         font-weight: 400
@@ -98,7 +103,7 @@ const onInput = (e: Event) => {
         height: 17px
         border-radius: 3px
         border: 1px solid #bbbbbb
-        font-size: 16px
+        font-size: 14px
         color: #666666
     ._email_label
         width: 103px
@@ -110,10 +115,17 @@ const onInput = (e: Event) => {
         height: 17px
         font-size: 14px
         margin-bottom: 5px
-    ._email_input
+    ._email_input 
         padding: 11.5px 12px 11.5px 12px
     ._password_input
         padding: 11.5px 10px 11.5px 12px
+        color: #666
+        font-family: "Noto Sans JP"
+        font-size: 0.875rem
+        font-style: normal
+        font-weight: 400
+        line-height: normal
+        letter-spacing: 0.04375rem
     ._eye_button
         position: absolute
         right: 12px
@@ -125,7 +137,6 @@ const onInput = (e: Event) => {
         padding: 0
         margin: 0
         height: 18px
-
     ._input_password_form
         position: relative
     ._opened_eye_icon
@@ -136,4 +147,13 @@ const onInput = (e: Event) => {
         display: block
         width: 18px
         height: 18.36px
+    ._password_masked
+        -webkit-text-security: disc  
+        font-size: 13px              
+        letter-spacing: 0.02em       
+        padding-top: 15px
+        padding-bottom: 8px
+        line-height: 16px
+    ._error_color
+        background: #FFD8D8
 </style>
--- src/pages/TDLogin.vue
+++ src/pages/TDLogin.vue
@@ -6,28 +6,21 @@ import TDInput from '@/components/TDInput/TDInput.vue';
 const email = ref('');
 const password = ref('');
 const visible = ref(false); 
-
-//import openedEye from '@/assets/TDInputOpenedEye.svg'
-//import closedEye from '@/assets/TDInputClosedEye.svg'
-
-
 </script>
 
 <template>
     <h1>このページはTDLogin.vueです。</h1>
     <button @click="$router.push('/home')">Homeに戻る</button>
     <TDMainMark src="../public/images/TDMainMark.svg"/>
-    <TDInput v-model="email" type="mail" />
+    <TDInput v-model="email" type="mail" :error="true"/>
     <TDInput 
       v-model="password" 
+      v-model:visible="visible"
       type="password" 
-      :visible="visible"
       openedEyeSrc="../public/images/TDInputOpenedEye.svg"
       closedEyeSrc="../public/images/TDInputClosedEye.svg"
-      @toggleVisible="visible = !visible"
+      :error="true"
     />
-    
-{{ email }}
 </template>
 
 <style lang="sass" scoped>
