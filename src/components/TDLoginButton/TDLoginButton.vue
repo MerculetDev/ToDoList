bbf5c0020763a94f5e27d02f7927833aebe6b23e
Atsuya Nishiyama <a-nishiyama@mldfp.com>
2025-10-11 10:23:39 +0900

    ボタンがクリックされた際のstyleの修正

--- src/components/TDLoginButton/TDLoginButton.vue
+++ src/components/TDLoginButton/TDLoginButton.vue
@@ -10,10 +10,14 @@ const props = defineProps<LoginButtonProps>()
 </script>
 
 <template>
-  <div class="_login_button">
-    <button class="_login_button_wrapper">
-      <div class="_login_button_container">{{ props.buttonText }}</div>
-      <img :src="props.src" alt="ログインボタンの画像" />
+  <div class="_login_button_wrapper">
+    <button class="_login_button_container">
+      <div class="_login_button_text">{{ props.buttonText }}</div>
+      <div
+        class="_login_button_icon"
+        :style="{ '--icon-url': `url(${props.src})` }"
+      >
+      </div>
     </button>
   </div>
 </template>
@@ -32,7 +36,7 @@ button
   -moz-appearance: none
   appearance: none
   cursor: pointer
-._login_button
+._login_button_wrapper
   display: grid    //gridレイアウトを使用
   place-items: center //中央に配置
   width: 100%
@@ -44,22 +48,35 @@ button
   line-height: normal
   letter-spacing: 2.4px
   border-radius: 999px
-  &:active 
+  &:active
     background-color: #FFFFFF
-    color: #11ABC1
-    border-radius: 999px
     border: 1px solid #11ABC1
-._login_button_wrapper
+    ._login_button_text
+      color: #11ABC1
+    ._login_button_icon
+      background-color: #11ABC1
+._login_button_container
   display: grid
   align-items: center
   justify-content: center
   grid-template-columns: max-content max-content
   gap: 10px
-._login_button_container
+._login_button_text
   display: grid
   place-items: center
   width: 100%
-  color: #FFFFFF
   letter-spacing: 2.4px
-
+  color: white
+._login_button_icon
+  width: 16px
+  height: 16px
+  background-color: #fff        /* 通常色（ここが“色”） */
+  -webkit-mask-image: var(--icon-url) 
+  mask-image: var(--icon-url)
+  -webkit-mask-repeat: no-repeat
+  mask-repeat: no-repeat
+  -webkit-mask-position: center
+  mask-position: center
+  -webkit-mask-size: contain
+  mask-size: contain
 </style>
