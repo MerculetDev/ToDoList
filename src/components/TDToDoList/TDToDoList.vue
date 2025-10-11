a3a7aed4ad8a201d099605d490436eacf4deeb0f
Atsuya Nishiyama <a-nishiyama@mldfp.com>
2025-10-11 17:16:25 +0900

    vueの新規作成とToDoリストの土台作成

---
+++ src/components/TDToDoList/TDToDoList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+interface ToDoItem {
+  text: string;
+  completed: boolean;
+  src: string;
+}
+const model = withDefaults(defineProps<ToDoItem>(), {});
+
+const addItems = () => {};
+
+onMounted(() => {
+  console.log(model.src);
+});
+</script>
+
+<template>
+  <div class="_todo_list">
+    <button @click="addItems">ボタン</button>
+    <div v-for="item in 5" :key="item" class="_todo_item">
+      <input type="checkbox" id="todo1" name="todo1" value="todo1" />
+      <input
+        type="text"
+        id="todo1_text"
+        name="todo1_text"
+        placeholder="タスクを入力"
+      />
+      <img :src="src" alt="ゴミ箱アイコン" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="sass"></style>
--- src/pages/TDHome.vue
+++ src/pages/TDHome.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TDMainMark from '@/components/TDMainMark/TDMainMark.vue';
+import TDMainMark from "@/components/TDMainMark/TDMainMark.vue";
+import TDToDoList from "@/components/TDToDoList/TDToDoList.vue";
 </script>
 
 <template>
-    <h1>このページはTDHome.vueです。</h1>
-    <button @click="$router.push('/login')">Logoutする</button>
-    <TDMainMark src="../public/images/TDMainMark.svg"/>
+  <h1>このページはTDHome.vueです。</h1>
+  <button @click="$router.push('/login')">Logoutする</button>
+  <TDMainMark src="images/TDMainMark.svg" />
+  <TDToDoList src="images/TDToDoListIcon.svg" />
 </template>
